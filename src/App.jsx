d463e9945a65690faee6cd6ee5280f22a849ac97
topgirl2410 <maria.topgirl82@gmail.com>
2023-01-20 15:16:11 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Headers.jsx"
+import Formulario from "./components/Formulario.jsx"
+import ListadoPacientes from "./components/ListadoPacientes.jsx"
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Formulario />
+      <ListadoPacientes />
+
+    </>
+  )
+}
+
+export default App
